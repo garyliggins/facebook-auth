b9dc8a9555d11634b572f1edbad1722c1f44e3ad
gary <gary@ligginsdevelopment.com>
2021-01-12 12:32:30 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import './App.css';
+import Facebook from './components/Facebook';
+function App() {
+  return (
+    <div className="App">
+      <h2>To sign in Authentic with Facebook</h2>
+      <Facebook/>
+    </div>
+  );
+}
+
+export default App;
